0a77551314cfd9c7998e7855a608fda915101b0b
heldic <helge@dickel.io>
2019-07-11 14:12:45 +0200

    Propagate error on docker build in custom skaffold builder

--- scripts/custom-cr-build.sh
+++ scripts/custom-cr-build.sh
@@ -9,14 +9,30 @@
 ## This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file (https://github.com/SAP/ewm-cloud-robotics/blob/master/LICENSE)
 ##
 
+cleanup() {
+    rv=$?
+    rm -rf "$tmpdir"
+    exit $rv
+}
+
+tmpdir="$(mktemp)"
+trap "cleanup" INT TERM EXIT
+# Do things...
+
+
 tag=$(date +%s)
 
 loc=$(pwd)
 cd $3
 
 docker build -f $2 -t $1:"dirty" -t $CR"/"$1:$tag -t $CR"/"$1":latest" .
+if [ $? -eq 0 ] 
+then
+    docker push $CR"/"$1:$tag 
+    docker push $CR"/"$1":latest"
 
-docker push $CR"/"$1:$tag 
-docker push $CR"/"$1":latest"
-
-cd "$loc"
+    cd "$loc"
+    exit 0
+else
+    exit 1
+fi
